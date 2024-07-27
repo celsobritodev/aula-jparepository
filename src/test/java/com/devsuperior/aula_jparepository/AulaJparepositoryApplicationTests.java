d49bebd8d273c40af0a40c23aa9a1e25b4536f53
celsobritodev <celsinhobrito@gmail.com>
2024-07-27 18:40:07 -0300

    aula jparepository devsuperior

---
+++ src/test/java/com/devsuperior/aula_jparepository/AulaJparepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.aula_jparepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaJparepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
